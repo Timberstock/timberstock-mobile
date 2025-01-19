b128cf8040bfba0bba8ef310b99df0ae06a10d4e
Mateo <maormeno@uc.cl>
2025-01-19 06:55:08 -0300

    upgrade Expo SDK to 52

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,11 @@ import {
   Text,
   TouchableOpacity,
   ImageBackground,
-  // Platform,
+  Platform,
+  StatusBar,
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
-// import Constants from "expo-constants";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import colors from "@/resources/Colors";
 
@@ -19,6 +20,7 @@ interface HeaderProps {
 }
 
 export default function Header(props: HeaderProps) {
+  const insets = useSafeAreaInsets();
   const { empresa, screenName, navigation } = props;
 
   const handleBack = () => {
@@ -26,26 +28,36 @@ export default function Header(props: HeaderProps) {
   };
 
   return (
-    <ImageBackground
-      source={require("../../assets/header_gradient.png")} // Put your gradient image here
-      style={styles.container}
-    >
-      <View style={styles.innerContainer}>
-        {screenName !== "Home" && (
-          <TouchableOpacity style={styles.backButton} onPress={handleBack}>
-            <Icon name="arrow-back" size={28} color={colors.white} />
-          </TouchableOpacity>
-        )}
-        <Text style={styles.titleText}>{empresa || "TimberStock"}</Text>
-      </View>
-    </ImageBackground>
+    <>
+      <StatusBar
+        translucent
+        backgroundColor="transparent"
+        barStyle="light-content"
+      />
+      <ImageBackground
+        source={require("../../assets/header_gradient.png")}
+        style={[
+          styles.container,
+          {
+            paddingTop: Platform.OS === 'ios' ? insets.top : StatusBar.currentHeight,
+          }
+        ]}
+      >
+        <View style={styles.innerContainer}>
+          {screenName !== "Home" && (
+            <TouchableOpacity style={styles.backButton} onPress={handleBack}>
+              <Icon name="arrow-back" size={28} color={colors.white} />
+            </TouchableOpacity>
+          )}
+          <Text style={styles.titleText}>{empresa || "TimberStock"}</Text>
+        </View>
+      </ImageBackground>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    // paddingTop: Platform.OS === "ios" ? Constants.statusBarHeight : 40,
-    paddingTop: 40,
     paddingBottom: 10,
     paddingHorizontal: 15,
     flexDirection: "row",
